1b1fa250b735d2cbde2b1eb30ad2560efe6bf5dc
Alexandre Mattos <alemattos_32@hotmail.com>
2025-06-23 17:44:44 -0300

    Service layer, component registration

---
+++ src/main/java/com/amattosmoraes/course/services/UserService.java
@@ -0,0 +1,25 @@
+package com.amattosmoraes.course.services;
+
+import com.amattosmoraes.course.entities.User;
+import com.amattosmoraes.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj =  repository.findById(id);
+        return obj.get();
+    }
+}
