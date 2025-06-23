3beb692435ed43121a6151efed2b30762129dafa
Alexandre Mattos <alemattos_32@hotmail.com>
2025-06-23 16:39:40 -0300

    User entity and resource

---
+++ src/main/java/com/amattosmoraes/course/entities/User.java
@@ -0,0 +1,78 @@
+package com.amattosmoraes.course.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+
+    private static final Long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+    private String fone;
+    private String email;
+    private String password;
+
+    public User(){
+    }
+
+    public User(Long id, String name, String fone, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.fone = fone;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFone() {
+        return fone;
+    }
+
+    public void setFone(String fone) {
+        this.fone = fone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(getId(), user.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+}
